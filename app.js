172ce2b398ca9a8d98cba4ab1457a54afff8a780
Istiyak Milon <milon.istiyak@gmail.com>
2018-11-30 01:06:39 +0600

    Express router used

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+// Adding 404 error page
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>');
+});
+
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-// POST Request
 router.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  console.log(req.body);
+  res.redirect('/');
 });
-
 
 module.exports = router;
