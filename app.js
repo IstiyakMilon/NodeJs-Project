9b43c99a1e128b60ed294140bfca3768625df3e6
Istiyak Milon <milon.istiyak@gmail.com>
2018-11-30 02:56:16 +0600

    Css files are serving staticaly with public folder

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,11 +9,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes); // admin is used for filtering paths
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// Adding 404 error page
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
