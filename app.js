f73eb2b0b779c1fe946d0ffc096d6d079dcf7c5b
Istiyak Milon <milon.istiyak@gmail.com>
2018-11-30 02:05:45 +0600

    404 page added and shop.html and add-product.html added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -13,7 +14,7 @@ app.use(shopRoutes);
 
 // Adding 404 error page
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,9 +7,7 @@ const router = express.Router();
 
 // /admin/add-product => GET Request
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 
